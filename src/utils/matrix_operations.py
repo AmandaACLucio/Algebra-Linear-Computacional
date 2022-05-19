ba3740b6dae9bf908105254d48b5b459cbeb1ab5
AmandaACLucio <amandalucio@poli.ufrj.br>
2022-05-19 17:34:27 -0300

    Create multiply matrix matrix

--- src/utils/matrix_operations.py
+++ src/utils/matrix_operations.py
@@ -14,17 +14,29 @@ def multiply_matrix_vector(vector, matrix):
 
     return result
 
-def multiply_matrix_vector(vector, matrix):
+def multiply_matrix_scalar(scalar, matrix):
     
-    if(len(vector)!=len(matrix[0])):
-        return -1
-
-    result = np.zeros(range(matrix))
+    result = []
     for i in range(len(matrix)):
-        sum = 0
+        line=[]
         for j in range(len(matrix[0])):
-            sum+=matrix[i][j]*vector[j]
-        result[i]=sum
+            line.append(scalar*matrix[i][j])
+        result.append(line)
 
     return result
 
+def multiply_matrix_matrix(matrix1, matrix2):
+
+    if(len(matrix1[0])!=len(matrix2)):
+        return -1
+
+    result=[]
+    for i in range(len(matrix1)):
+        line=[]
+        for j in range(len(matrix2[0])):
+            sum=0
+            for k in range(len(matrix2)):
+                sum += matrix1[i][k] * matrix2[k][j]
+            line.append(sum)
+        result.append(line)
+    return result
