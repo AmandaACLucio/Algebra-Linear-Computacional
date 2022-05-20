50f7600bfea437a07c95224653def9861ac25cc6
nay.gomes <nayara.gomes@g.globo>
2022-05-20 00:42:50 -0300

    criando função que formata matrix

--- src/utils/files_operations.py
+++ src/utils/files_operations.py
@@ -1,13 +1,26 @@
 import json
 
+
 def readTxtFile(path):
     with open(path, 'r') as f:
         content = f.readlines()
     return content
+
 
 def readJsonFile(path):
     with open(path) as f:
         content = json.load(f)
     return content
 
-readTxtFile("C:/Algebra-Linear-Computacional/src/task 1/file.txt")
+
+def formatMatrix(matrix):
+    newMatrix = []
+    for i in matrix:
+        newLine = i.replace('\n', '').split(' ')
+        for j in range(len(newLine)):
+            newLine[j] = float(newLine[j])
+        newMatrix.append(newLine)
+    print(matrix)
+    return newMatrix
+
+print(formatMatrix(readTxtFile("C:/Algebra-Linear-Computacional/src/task 1/file.txt")))
