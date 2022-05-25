c549d21e1938a91e2568e7c887d7cf1dfed6bdd1
nay.gomes <nayara.gomes@g.globo>
2022-05-24 23:17:48 -0300

    metodo gauss seidel

---
+++ src/task_1/iterative_procedure/Gauss_Seidel.py
@@ -0,0 +1,50 @@
+from src.utils.matrix_operations import converges, is_square_matrix, value_residue
+
+
+def gauss_seidel(matrix, vector_b, max_tolerance):
+
+    lines = len(matrix)
+
+    if(not is_square_matrix(matrix)):
+        print("A matriz precisa ser quadrada para realizar essa operação")
+        return -1
+
+    if(not converges(matrix)):
+        print("A matriz não converge para o método jacobiano")
+        return -1
+
+    vector_old = [1 for x in range(lines)]
+    vector_new = [0 for x in range(lines)]
+    residue = 1
+    residue_historic = [1]
+    step = 0
+
+    while(residue >= max_tolerance):
+        for i in range(lines):
+            vector_new[i] = vector_b[i]
+
+            for j in range(lines):
+
+                if(i > j):
+                    vector_new[i] -= matrix[i][j]*vector_new[j]
+                
+                if(i < j):
+                    vector_new[i] -= matrix[i][j]*vector_old[j]
+
+            vector_new[i] /= matrix[i][i]
+
+        residue = value_residue(vector_new, vector_old)
+        residue_historic.append(residue)
+
+        for i in range(lines):
+            vector_old[i] = vector_new[i]
+
+        step += 1
+
+    return [vector_new, residue_historic, step]
+
+print(gauss_seidel([
+    [10, 2, 3],
+    [1, 5, 1],
+    [2, 3, 10],
+], [7, -8, 6], 0.05))
