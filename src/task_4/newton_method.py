9c6a0b68389d9e66cbfd4d4bd2768101ded92389
nay.gomes <nayara.gomes@g.globo>
2022-06-28 21:24:50 -0300

    newton method - not finished

--- src/task_4/newton_method.py
+++ src/task_4/newton_method.py
@@ -1,16 +1,18 @@
 import math
-from src.utils.matrix_operations import get_jacobian_matrix, inverse_matrix, multiply_matrix_vector, sum_vectors, norm_vector, get_vector_f
+from src.utils.matrix_operations import calc_jacobiano, inverse_matrix, multiply_matrix_vector, add_vector_vector, norm_vector, get_vector_f, value_function
 
 STEPS = 1000
 TOL = 10**-4
 
 
-def newton_method_nl(equations, x0):
-    x = x0
+def newton_method_nl(theta1, theta2):
+    x = [1, 0, 0]
+
+    equations = value_function(x, theta1, theta2)
 
     for _ in range(STEPS):
         jacobian_inverse = inverse_matrix(
-            get_jacobian_matrix(equations, x))
+            calc_jacobiano(equations, x))
         vector_f = get_vector_f(equations, x)
 
         if(jacobian_inverse == 0):
@@ -19,7 +21,7 @@ def newton_method_nl(equations, x0):
 
         delta_x = multiply_matrix_vector(jacobian_inverse, vector_f)
         delta_x = [x*(-1) for x in delta_x]
-        x = sum_vectors(x, delta_x)
+        x = add_vector_vector(x, delta_x)
 
         residue = norm_vector(delta_x)/norm_vector(x)
 
