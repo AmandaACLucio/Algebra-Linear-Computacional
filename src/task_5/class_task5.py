e7800164469e1e89e5b1cd694f4ffcd9973c3db5
nay.gomes <nayara.gomes@g.globo>
2022-06-29 03:17:45 -0300

    colocando condicional icod

--- src/task_5/class_task5.py
+++ src/task_5/class_task5.py
@@ -42,5 +42,12 @@ class task_5:
 
                 content['solution'] = values_c
                 content['useErrors'] = use_errors
+            elif(self.ICOD == 2):
+                print("Integral")
+
+            elif(self.ICOD == 3):
+                print("Derivada DF")
+            elif(self.ICOD == 4):
+                print("Derivada RE")
 
             write_output_file(content)
